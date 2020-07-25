3f80510cd2fea0cd81d28eadf451e49003527852
Dannus90 <persson.daniel.1990@gmail.com>
2020-07-25 16:31:34 +0200

    Add News feed, upload post functionality and delete post button (no functioniality yet)

--- components/index/PostFeed.js
+++ components/index/PostFeed.js
@@ -1,21 +1,101 @@
-// import Typography from "@material-ui/core/Typography";
+import Typography from "@material-ui/core/Typography";
 import withStyles from "@material-ui/core/styles/withStyles";
-
+import NewPost from "./NewPost";
+import Post from "./Post";
+import { addPost, getPostFeed } from "../../lib/api";
 class PostFeed extends React.Component {
-  state = {};
+    state = {
+        posts: [],
+        text: "",
+        image: "",
+        isAddingPost: false,
+    };
 
-  render() {
-    return <div>PostFeed</div>;
-  }
+    componentDidMount() {
+        this.postData = new FormData();
+        this.getPosts();
+    }
+
+    getPosts = () => {
+        const { auth } = this.props;
+
+        getPostFeed(auth.user._id).then((posts) => {
+            return this.setState({ posts });
+        });
+    };
+
+    handleChange = (e) => {
+        let inputValue;
+
+        if (e.target.name === "image") {
+            inputValue = e.target.files[0];
+        } else {
+            inputValue = e.target.value;
+        }
+
+        this.postData.set(e.target.name, inputValue);
+        this.setState({ [e.target.name]: inputValue });
+    };
+
+    handleAddPost = () => {
+        const { auth } = this.props;
+
+        this.setState({ isAddingPost: true });
+        addPost(auth.user._id, this.postData)
+            .then((postData) => {
+                const updatedPosts = [postData, ...this.state.posts];
+                this.setState({
+                    posts: updatedPosts,
+                    isAddingPost: false,
+                    text: "",
+                    image: "",
+                });
+                this.postData.delete("image");
+            })
+            .catch((err) => {
+                console.error(err);
+                this.setState({ isAddingPost: false });
+            });
+    };
+
+    render() {
+        const { classes, auth } = this.props;
+        const { posts, text, image, isAddingPost } = this.state;
+        console.log(posts);
+        return (
+            <div className={classes.root}>
+                <Typography
+                    variant="h4"
+                    component="h1"
+                    align="center"
+                    color="primary"
+                    className={classes.title}
+                >
+                    Post Feed
+                </Typography>
+                <NewPost
+                    auth={auth}
+                    text={text}
+                    image={image}
+                    isAddingPost={isAddingPost}
+                    handleChange={this.handleChange}
+                    handleAddPost={this.handleAddPost}
+                />
+                {posts.map((post) => (
+                    <Post key={post._id} auth={auth} post={post} />
+                ))}
+            </div>
+        );
+    }
 }
 
-const styles = theme => ({
-  root: {
-    paddingBottom: theme.spacing.unit * 2
-  },
-  title: {
-    padding: theme.spacing.unit * 2
-  }
+const styles = (theme) => ({
+    root: {
+        paddingBottom: theme.spacing.unit * 2,
+    },
+    title: {
+        padding: theme.spacing.unit * 2,
+    },
 });
 
 export default withStyles(styles)(PostFeed);
